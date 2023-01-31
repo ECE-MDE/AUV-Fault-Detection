662c147b5334bfe0d150209edf53787380203ab1
Stephen Welch <stephenwelchva@vt.edu>
2023-01-31 15:09:41 -0500

    Simulation starts

--- avl/setup.sh
+++ avl/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 git config --global credential.helper store
+ln -s /workspaces/AUV-Fault-Detection/avl /root/avl
 wstool init src avl.rosinstall
 rosdep install --from-paths . --ignore-src -y
-echo "alias avl='/workspaces/AUV-Fault-Detection/avl/src/avl_tools/scripts/avl.sh'" >> ~/.bashrc && source ~/.bashrc
+echo "alias avl='/workspaces/AUV-Fault-Detection/avl/src/avl_tools/scripts/avl.sh'" >> ~/.bashrc
+echo "alias find='find -L'" >> ~/.bashrc
+source ~/.bashrc
+source /opt/ros/noetic/setup.bash
