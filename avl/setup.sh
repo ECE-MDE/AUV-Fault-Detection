47f87aa43f3094cc81cf261ef2f53933459ab9c3
Stephen Welch <stephenwelchva@vt.edu>
2023-02-20 03:01:14 -0500

    Add sourcing ROS to setup

--- avl/setup.sh
+++ avl/setup.sh
@@ -7,6 +7,7 @@ sudo apt update
 rosdep install --from-paths . --ignore-src -y
 
 echo "alias avl='/workspaces/AUV-Fault-Detection/avl/src/avl_tools/scripts/avl.sh'" >> ~/.bashrc
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 # echo "alias find='find -L'" >> ~/.bashrc
 source ~/.bashrc
 source /opt/ros/noetic/setup.bash
