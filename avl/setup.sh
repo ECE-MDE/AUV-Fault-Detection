f1eff01a67b9404cc960afb1aa60c05f54cdbf4b
Stephen Welch <stephenwelch@vt.edu>
2023-03-15 04:17:14 -0400

    Workspace config updates

--- avl/setup.sh
+++ avl/setup.sh
@@ -8,6 +8,7 @@ rosdep install --from-paths . --ignore-src -y
 
 echo "alias avl='/workspaces/AUV-Fault-Detection/avl/src/avl_tools/scripts/avl.sh'" >> ~/.bashrc
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+echo "source /workspaces/AUV-Fault-Detection/avl/devel/setup.bash" >> ~/.bashrc
 # echo "alias find='find -L'" >> ~/.bashrc
 source ~/.bashrc
 source /opt/ros/noetic/setup.bash
