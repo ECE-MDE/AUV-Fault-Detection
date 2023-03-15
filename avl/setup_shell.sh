c8e968a2e5940302347aed6bcbb4b20012f414f5
Stephen Welch <stephenwelchva@vt.edu>
2023-03-15 19:15:56 -0400

    Workspace improvements

---
+++ avl/setup_shell.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+echo "alias avl='/workspaces/AUV-Fault-Detection/avl/src/avl_tools/scripts/avl.sh'" >> ~/.bashrc
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+echo "source /workspaces/AUV-Fault-Detection/avl/devel/setup.bash" >> ~/.bashrc
