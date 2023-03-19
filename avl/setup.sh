31026b3f67a930915f20c6999d5e4fcd61515148
Stephen Welch <stephenwelchva@vt.edu>
2023-03-18 23:02:28 -0400

    Add pip installs to setup

--- avl/setup.sh
+++ avl/setup.sh
@@ -3,4 +3,6 @@
 git config --global credential.helper store
 wstool init src avl.rosinstall
 sudo apt update
+sudo apt install -y python3-pip
+pip install roslibpy
 rosdep install --from-paths . --ignore-src -y
